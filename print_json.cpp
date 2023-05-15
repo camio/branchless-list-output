56383a88fc72903ed8a258be1217d80739c37bf7
David Sankel <dsankel@adobe.com>
2023-05-15 12:41:23 -0400

    fix spacing

--- print_json.cpp
+++ print_json.cpp
@@ -5,25 +5,25 @@ void printInt(int);
 
 void print_json(int * pBegin, int *pEnd)
 {
-    bool saw = false;
-    for( int * iter = pBegin; iter != pEnd; iter++ ) {
-        if( !saw )
-            printChar('[');
-        else
-            printChar(',');
-        printInt(*iter);
-        saw = true;
-    }
-    printChar(']');
+  bool saw = false;
+  for( int * iter = pBegin; iter != pEnd; iter++ ) {
+    if( !saw )
+      printChar('[');
+    else
+      printChar(',');
+    printInt(*iter);
+    saw = true;
+  }
+  printChar(']');
 }
 
 void print_json_branchless(int * pBegin, int *pEnd)
 {
-    char y = '[';
-    for( int * iter = pBegin; iter != pEnd; iter++ ) {
-        char z = ',';
-        printChar(std::exchange(y,z));
-        printInt(*iter);
-    }
-    printChar(']');
+  char y = '[';
+  for( int * iter = pBegin; iter != pEnd; iter++ ) {
+    char z = ',';
+    printChar(std::exchange(y,z));
+    printInt(*iter);
+  }
+  printChar(']');
 }
